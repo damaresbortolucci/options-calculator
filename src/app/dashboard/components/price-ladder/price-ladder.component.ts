37ecfe0acd86cb0f5351c886eae40551a72b834e
damaresbs <damares_sp78@yahoo.com.br>
2026-02-16 19:44:22 -0300

    ajusta layout para B/V points em price-ladder

--- src/app/dashboard/components/price-ladder/price-ladder.component.ts
+++ src/app/dashboard/components/price-ladder/price-ladder.component.ts
@@ -22,4 +22,14 @@ export class PriceLadderComponent {
   getReversedVPoints() {
     return [...(this.criticalPoints?.vPoints || [])].reverse();
   }
+
+  getBPointTop(idx: number): number {
+    const isMobile = window.innerWidth < 768;
+    return isMobile ? 2 + idx * 13 : 8 + idx * 13;
+  }
+
+  getVPointTop(idx: number): number {
+    const isMobile = window.innerWidth < 768;
+    return isMobile ? 58 + idx * 13 : 58 + idx * 13;
+  }
 }
